50eaf3d0a302e8d2aca0bf057441c3095f5e9cec
ampirzadeh <am.pirzadeh@gmail.com>
2025-04-18 16:50:33 +0000

    Total theme change to onedark from catppuccin

--- zsh/my.zsh
+++ zsh/my.zsh
@@ -57,12 +57,33 @@ eval "$(zoxide init zsh --cmd cd)"
 export TERMINAL="ghostty"
 export SUDO_EDITOR="$EDITOR"
 
-export FZF_DEFAULT_OPTS=" \
-  --color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 \
-  --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
-  --color=marker:#b4befe,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8 \
-  --color=selected-bg:#45475a \
-  --multi"
+_gen_fzf_default_opts() {
+
+local color00='#282c34'
+local color01='#353b45'
+local color02='#3e4451'
+local color03='#545862'
+local color04='#565c64'
+local color05='#abb2bf'
+local color06='#b6bdca'
+local color07='#c8ccd4'
+local color08='#e06c75'
+local color09='#d19a66'
+local color0A='#e5c07b'
+local color0B='#98c379'
+local color0C='#56b6c2'
+local color0D='#61afef'
+local color0E='#c678dd'
+local color0F='#be5046'
+
+export FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS"\
+" --color=bg+:$color01,bg:$color00,spinner:$color0C,hl:$color0D"\
+" --color=fg:$color04,header:$color0D,info:$color0A,pointer:$color0C"\
+" --color=marker:$color0C,fg+:$color06,prompt:$color0A,hl+:$color0D"
+
+}
+
+_gen_fzf_default_opts
 
 # Allow Ctrl-z to toggle between suspend and resume 
 function Resume {  
