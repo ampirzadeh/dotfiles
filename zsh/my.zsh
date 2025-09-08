d75827243fc4686635281532794720e65c7eaa45
ampirzadeh <am.pirzadeh@gmail.com>
2025-09-08 19:39:16 +0100

    Slight eza changes

--- zsh/my.zsh
+++ zsh/my.zsh
@@ -83,6 +83,8 @@ export FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS"\
 
 }
 
+export FZF_COMPLETION_DIR_COMMANDS="cd pushd rmdir tree"
+
 _gen_fzf_default_opts
 
 # Allow Ctrl-z to toggle between suspend and resume 
@@ -121,9 +123,8 @@ bindkey '^G' open_neovide_to_workspace
 # https://gist.github.com/AppleBoiy/04a249b6f64fd0fe1744aff759a0563b
 alias ls='eza --color=always --group-directories-first --icons'
 alias ll='eza -la --icons --octal-permissions --group-directories-first'
-alias l='eza -bGF --header --git --color=always --group-directories-first --icons'
+alias l='eza -bGF --oneline --header --git --color=always --group-directories-first --icons'
 alias llm='eza -lbGd --header --git --sort=modified --color=always --group-directories-first --icons' 
 alias la='eza --long --all --group --group-directories-first'
 alias lx='eza -lbhHigUmuSa@ --time-style=long-iso --git --color-scale --color=always --group-directories-first --icons'
-alias lS='eza -1 --color=always --group-directories-first --icons'
 alias lt='eza --tree --level=2 --color=always --group-directories-first --icons'
