16f633a30e3edffe763a77ea2280661e82879188
Amir Pirzadeh <am.pirzadeh@gmail.com>
2026-09-04 19:49:48 +0100

    path entries

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -3,7 +3,9 @@
 # Environment
 set -gx MANPAGER "sh -c 'sed -u -e \"s/\\x1B\[[0-9;]*m//g; s/.\\x08//g\" | bat -p -lman'"
 
-set -gx PATH $PATH "$HOME/.local/zed.app/bin"
+fish_add_path "/opt/nvim/bin"
+fish_add_path "$HOME/.local/zed.app/bin"
+fish_add_path "$HOME/.opencode/bin"
 
 # >>> oh-my-opencode-slim background subagents >>>
 set -gx OPENCODE_EXPERIMENTAL_BACKGROUND_SUBAGENTS true
